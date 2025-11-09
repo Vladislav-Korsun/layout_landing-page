70db818b8d09c14ca8cdde2a1680daca77c00c26
Vladislav-Korsun <vladosik385@gmail.com~>
2025-11-09 15:19:07 +0200

    update: contact section improvements & hover animations

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('contactForm');
+
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    form.reset();
+  });
+});
