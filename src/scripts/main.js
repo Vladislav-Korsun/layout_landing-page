d867989806a01c4d7906b8f6e4205c1f0213f505
Vladislav-Korsun <vladosik385@gmail.com~>
2025-11-10 15:30:05 +0200

    add fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,1 @@
 'use strict';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const form = document.getElementById('contactForm');
-
-  form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    form.reset();
-  });
-});
